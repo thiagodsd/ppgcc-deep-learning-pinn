0cd500d8e9e050b50906fc8e33ccf60743086b0c
dsthiago <thiagosd.2.18@gmail.com>
2023-11-04 10:05:21 -0300

    `newton_law_cooling.ipynb` refactored

--- src/newton_law_cooling.py
+++ src/newton_law_cooling.py
@@ -1,33 +1,20 @@
+import datetime
+import gc
+import json
 import os
+import pprint
 import warnings
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import optuna
 import pandas as pd
 import seaborn as sns
-import tqdm
-
-import optuna
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import tqdm
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import ParameterGrid, train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
-from IPython.display import display
-
-mpl.rcParams.update(mpl.rcParamsDefault)
-warnings.filterwarnings("ignore")
-
-# %matplotlib inline
-
-mpl.rcParams["font.family"] = "serif"
-
-experiment_name = "nlc_with_param_learning"
-folder_path = f"../data/08_reporting/{experiment_name}"
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-
-golden_ratio = (np.sqrt(5) - 1) / 2
