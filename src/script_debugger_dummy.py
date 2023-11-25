613e552e118417b5f24edb94f03de7cc5499bba5
dsthiago <thiagosd.2.18@gmail.com>
2023-11-25 07:38:43 -0300

    Add scikit-image library and update gitignore file

--- src/script_debugger_dummy.py
+++ src/script_debugger_dummy.py
@@ -6,10 +6,14 @@ import pprint
 import warnings
 from io import BytesIO
 
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import folium
 import geopandas as gpd
+import h5py
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import netCDF4 as nc
 import numpy as np
 import optuna
 import pandas as pd
@@ -20,7 +24,9 @@ import torch.optim as optim
 import tqdm
 from folium.plugins import HeatMap
 from IPython.display import display
+from scipy.interpolate import griddata
 from selenium import webdriver
+from skimage.transform import resize
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import ParameterGrid, train_test_split
 from sklearn.preprocessing import MinMaxScaler
